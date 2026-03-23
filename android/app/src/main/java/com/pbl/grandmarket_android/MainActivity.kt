e1f50e0aa2722c8a59b375aa95b23e42d9bca438
김원혁 <kwh102455@gmail.com>
2026-03-24 08:58:07 +0900

    feat: MainActivity 뷰 바인딩 적용함

--- android/app/src/main/java/com/pbl/grandmarket_android/MainActivity.kt
+++ android/app/src/main/java/com/pbl/grandmarket_android/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.pbl.grandmarket_android
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.pbl.grandmarket_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
