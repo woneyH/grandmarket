96475bcc349b9621c2a0e71a3a363a77d804a3fc
김원혁 <kwh102455@gmail.com>
2026-03-31 17:24:55 +0900

    test: LoginActivity 화면 뷰 테스트함
     - 로그인 화면 테스트 코드임 통합 테스트

---
+++ android/app/src/androidTest/java/com/pbl/grandmarket_android/LoginActivityTest.kt
@@ -0,0 +1,26 @@
+package com.pbl.grandmarket_android
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun testLoginScreen_isDisplayed() {
+        onView(withText("로그인")).check(matches(isDisplayed()))
+
+        onView(withId(R.id.btnKakaoLogin)).check(matches(isDisplayed()))
+    }
+
+}
