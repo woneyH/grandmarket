7608cc3ed39c614735de8cb0d9ed265b42150744
김원혁 <kwh102455@gmail.com>
2026-04-11 14:01:34 +0900

    chore: 동적 상품 추가 기능을 위한 의존성 추가함

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,4 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 }
