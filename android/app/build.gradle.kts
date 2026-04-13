7b8d71d3b463a38d4e8ae4e66a3e2c7e4f579ecf
김원혁 <kwh102455@gmail.com>
2026-04-13 18:47:42 +0900

    chore: 카카오 로그인 기능 제공을 위한 프로젝트 설정 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
 }
+
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
+val kakaoNativeAppKey = properties.getProperty("KAKAO_NATIVE_APP_KEY") ?: ""
+
 
 android {
     namespace = "com.pbl.grandmarket_android"
@@ -18,6 +26,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
+        buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"${kakaoNativeAppKey}\"")
     }
 
     buildTypes {
@@ -33,8 +44,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    viewBinding.enable = true
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -47,9 +60,16 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity.ktx)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+
+    //kakao 로그인 의존성 추가
+    implementation(libs.kakao.user)
+    // Retrofit2 & Gson
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
 }
