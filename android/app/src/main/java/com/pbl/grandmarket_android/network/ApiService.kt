36bd7f8aaa5edaf9dee7db49ccea92a49e604ecc
김원혁 <kwh102455@gmail.com>
2026-04-16 19:38:49 +0900

    feat: 비동기 함수 추가함

--- android/app/src/main/java/com/pbl/grandmarket_android/network/ApiService.kt
+++ android/app/src/main/java/com/pbl/grandmarket_android/network/ApiService.kt
@@ -1,7 +1,6 @@
 
 package com.pbl.grandmarket_android.network
 
-import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -14,5 +13,5 @@ data class KakaoLoginResponse(val token: String, val userId: Long)
 // g: 카카오 로그인 API 인터페이스
 interface ApiService {
     @POST("/api/members/login/kakao")
-    fun kakaoLogin(@Body request: KakaoLoginRequest): Call<KakaoLoginResponse>
+    suspend fun kakaoLogin(@Body request: KakaoLoginRequest): retrofit2.Response<KakaoLoginResponse>
 }
