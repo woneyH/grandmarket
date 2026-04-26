f1282ad91ebf3d15208653acb9a2b49969ce977f
김원혁 <kwh102455@gmail.com>
2026-04-26 20:56:45 +0900

    feat: 데이터 클래스에 user 권한 role 필드 추가함

--- android/app/src/main/java/com/pbl/grandmarket_android/network/ApiService.kt
+++ android/app/src/main/java/com/pbl/grandmarket_android/network/ApiService.kt
@@ -4,13 +4,16 @@ package com.pbl.grandmarket_android.network
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-// g: 카카오 로그인 요청용 데이터 모델
-data class KakaoLoginRequest(val accessToken: String)
+// 카카오 로그인 요청용 데이터 모델
+data class KakaoLoginRequest(
+    val accessToken: String,
+    val role: String
+)
 
-// g: 카카오 로그인 응답 데이터 모델(예시)
+// 카카오 로그인 응답 데이터 모델
 data class KakaoLoginResponse(val token: String, val userId: Long)
 
-// g: 카카오 로그인 API 인터페이스
+// 카카오 로그인 API 인터페이스
 interface ApiService {
     @POST("/api/members/login/kakao")
     suspend fun kakaoLogin(@Body request: KakaoLoginRequest): retrofit2.Response<KakaoLoginResponse>
